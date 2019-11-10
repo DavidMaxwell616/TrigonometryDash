d96aa62d1509609a569b05fd249dd2ffa76e901e
DavidMaxwell616 <33132201+DavidMaxwell616@users.noreply.github.com>
2019-11-09 23:09:15 -0800

    updates

--- js/game.js
+++ js/game.js
@@ -16,7 +16,7 @@ const config = {
     default: 'arcade',
     arcade: {
       gravity: {
-        y: 500,
+        y: 800,
       },
     },
   },
@@ -34,49 +34,30 @@ var highScorePic;
 var pointer;
 
 function preload() {
-  // Image layers from Tiled can't be exported to Phaser 3 (as yet)
-  // So we add the background image separately
-  //this.load.image('background', 'assets/images/background.png');
-  // Load the tileset image file, needed for the map to know what
-  // tiles to draw on the screen
   this.load.image('tiles', 'assets/tilesets/grounds.png');
-  // Even though we load the tilesheet with the obstacle image, we need to
-  // load the obstacle image separately for Phaser 3 to render it
-
   this.load.image('score', 'assets/images/score.png');
   this.load.image('highscore', 'assets/images/HighScore.png');
-
   this.load.spritesheet('obstacles', 'assets/images/obstacles.png', {
     frameWidth: 64,
     frameHeight: 64,
   });
-  // Load the export Tiled JSON
   this.load.tilemapTiledJSON('map', 'assets/tilemaps/level1.json');
-
   this.load.path = '../assets/images/';
   this.load.spritesheet('player', 'cubes.png', {
     frameWidth: 138,
     frameHeight: 135,
   });
-
   this.load.spritesheet('background', 'backgrounds-hd.png', {
     frameWidth: 1024,
     frameHeight: 1024,
   });
-  // Load player animations from the player spritesheet and atlas JSON
-  // this.load.atlas('player', 'assets/images/kenney_player.png',
-  //   'assets/images/kenney_player_atlas.json');
 }
 
 function create() {
-  // Create a tile map, which is used to bring our level in Tiled
-  // to our game world in Phaser
   const map = this.make.tilemap({
     key: 'map',
   });
-  // Add the tileset to the map so the images would load correctly in Phaser
   const tileset = map.addTilesetImage('grounds', 'tiles');
-  // Place the background image in our game world
   var tint = Math.random() * 0xffffff;
   for (let index = 0; index <= 22; index++) {
     backgroundImage = this.add
@@ -96,49 +77,36 @@ function create() {
     true,
   );
 
-  // Add the platform layer as a static group, the player would be able
-  // to jump on platforms like world collisions but they shouldn't move
   tint = Math.random() * 0xffffff;
-  const platforms = map.createDynamicLayer('Platforms', tileset, 0, -500);
-  // There are many ways to set collision between tiles and players
-  // As we want players to collide with all of the platforms, we tell Phaser to
-  // set collisions for every tile in our platform layer whose index isn't -1.
-  // Tiled indices can only be >= 0, therefore we are colliding with all of
-  // the platform layer
-  platforms.setCollisionByExclusion(-1, true);
-  platforms.tint = tint;
+  this.platforms = map.createDynamicLayer('Platforms', tileset, 0, -500);
+  this.platforms.setCollisionByExclusion(-1, true);
+  this.platforms.tint = tint;
+  var layer = this.platforms;
 
-  // Add the player to the game world
   tint = Math.random() * 0xffffff;
   this.player = this.physics.add.sprite(50, 300, 'player');
-  this.player.setBounce(0.1); // our player will bounce from items
+  this.player.setBounce(0.1);
   this.player.tint = tint;
-  this.player.setCollideWorldBounds(true); // don't go out of the map
+  this.player.setCollideWorldBounds(true);
 
   this.player.setScale(0.5);
-  this.physics.add.collider(this.player, platforms);
+  this.physics.add.collider(this.player, this.platforms, playerHitPlatform, null, this);
+
   var camera = this.cameras.main;
   camera.setBounds(0, -500, map.widthInPixels, map.heightInPixels);
   camera.startFollow(this.player);
   camera.setFollowOffset(this.player.x - 200, this.player.y - 150);
-  // Enable user input via cursor keys
   this.cursors = this.input.keyboard.createCursorKeys();
 
   tint = Math.random() * 0xffffff;
-  // Create a sprite group for all obstacles, set common properties to ensure that
-  // sprites in the group don't move via gravity or by player collisions
   this.obstacles = this.physics.add.group({
     allowGravity: false,
     immovable: true,
   });
 
   var objTileset = map.getTileset('obstacles');
-  // Get the obstacles from the object layer of our Tiled map. Phaser has a
-  // createFromObjects function to do so, but it creates sprites automatically
-  // for us. We want to manipulate the sprites a bit before we use them
   const obstacleObjects = map.getObjectLayer('obstacles')['objects'];
   obstacleObjects.forEach(obstacleObject => {
-    // Add new obstacles to our sprite group
     const obstacle = this.obstacles
       .create(
         obstacleObject.x,
@@ -146,33 +114,30 @@ function create() {
         'obstacles',
       )
       .setOrigin(0, 0);
-    // By default the sprite has loads of whitespace from the base image, we
-    // resize the sprite to reduce the amount of whitespace used by the sprite
-    // so collisions can be more precise
     obstacle.body
       .setSize(obstacle.width, obstacle.height - 20)
       .setOffset(0, 20);
     var frame = obstacleObject.gid - objTileset.firstgid;
     obstacle.setFrame(frame);
     obstacle.spike = objTileset.tileProperties[frame];
+    if (obstacle.spike != null && !obstacle.spike.spike)
+      obstacle.body.enable = false;
   });
-  // Add collision between the player and the obstacles
-  this.physics.add.collider(this.player, this.obstacles, playerHit, null, this);
 
+  this.physics.add.collider(this.player, this.obstacles, playerHit, null, this);
   scorePic = this.add.image(50, 50, 'score').setOrigin(0, 0);
   scorePic.setScrollFactor(0);
-
   highScorePic = this.add.image(550, 50, 'highscore').setOrigin(0, 0);
   highScorePic.setScrollFactor(0);
-
-  scoreText = this.add.text(140, 58, score, {
+  scoreText = this.add.text(142, 57, score, {
     fontSize: '20px',
+    fontFamily: 'arial',
     fill: '#ffffff',
   });
   scoreText.setScrollFactor(0);
-
-  highScoreText = this.add.text(675, 58, highScore, {
+  highScoreText = this.add.text(677, 57, highScore, {
     fontSize: '20px',
+    fontFamily: 'arial',
     fill: '#ffffff',
   });
   highScoreText.setScrollFactor(0);
@@ -186,25 +151,38 @@ function updateText() {
 }
 
 function update() {
-  // Control the player with left or right keys
-  if (this.player.x > 400) backgroundImage.x += 13;
-  this.player.x += 5;
+  //if (this.player.x > 400) 
+  //backgroundImage.x += 13;
+
+  this.player.body.setVelocityX(250);
   score += 1;
   updateText();
 
-  if (this.player.body.onFloor()) this.player.angle = 0;
+  if (this.player.body.onFloor())
+    this.player.angle = 0;
   else this.player.angle += 3;
-
   if ((this.cursors.space.isDown || pointer.isDown || this.cursors.up.isDown) &&
     this.player.body.onFloor()
   ) {
-    this.player.setVelocityY(-350);
+    this.player.setVelocityY(-500);
+  }
+}
+
+function playerHitPlatform(player, platform) {
+
+  if (player.body.blocked.right) {
+    resetLevel(player, this);
   }
 }
 
 function playerHit(player, obstacle) {
+  console.log(player);
   if (!obstacle.spike.spike)
     return;
+  resetLevel(player, this);
+}
+
+function resetLevel(player, game) {
   // Set velocity back to 0
   player.setVelocity(0, 0);
   if (score > highScore) highScore = score;
@@ -217,11 +195,12 @@ function playerHit(player, obstacle) {
   // Set the visibility to 0 i.e. hide the player
   player.setAlpha(0);
   // Add a tween that 'blinks' until the player is gradually visible
-  let tw = this.tweens.add({
+  let tw = game.tweens.add({
     targets: player,
     alpha: 1,
     duration: 100,
     ease: 'Linear',
     repeat: 5,
   });
+
 }
