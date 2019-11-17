a8675e55e5e67987bf7123b6ed427a1c696b70d1
DavidMaxwell616 <33132201+DavidMaxwell616@users.noreply.github.com>
2019-11-16 21:40:31 -0800

    updates

--- js/game.js
+++ js/game.js
@@ -2,11 +2,7 @@ const config = {
   type: Phaser.AUTO,
   parent: 'game',
   width: 800,
-  heigth: 640,
-  scale: {
-    mode: Phaser.Scale.RESIZE,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-  },
+  height: 600,
   scene: {
     preload,
     create,
